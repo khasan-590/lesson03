567a64b60ebee0d427e4988673ea542026eec85d
khasan-590 <khasan4ik590@gmail.com>
2021-02-01 18:53:33 +0600

    изменил 3ий урок

--- js/script.js
+++ js/script.js
@@ -7,8 +7,8 @@ let  money = +prompt("Ваш месячный доход?", 1000) ,  //“Дох
   addExpenses =  prompt("Перечислите возможные расходы за рассчитываемый период через запятую"),
   deposit = confirm("Есть ли у вас депозит в банке?"), //любое булево значение,
   expenses1 = prompt('Введите обязательную статью расходов?'),
-  expenses2 = prompt('Введите обязательную статью расходов?'),
   amount1 = +prompt('Во сколько это обойдется?'),
+  expenses2 = prompt('Введите обязательную статью расходов?'),
   amount2 = +prompt('Во сколько это обойдется?'),
   mission = 50000, //любое число(Какую сумму хотите накопить)
   period = 6, //число от 1 до 12(месяцев)
@@ -29,10 +29,10 @@ let  money = +prompt("Ваш месячный доход?", 1000) ,  //“Дох
 
   if (budgetDay >= 1200) {
     console.log("У вас высокий уровень дохода");
-  } else if (budgetDay > 600, budgetDay < 1200) {
+  } else if (budgetDay > 600 && budgetDay < 1200) {
     console.log('У вас средний уровень дохода');
-  } else if (budgetDay <=600) {
+  } else if (budgetDay <=600 && budgetDay  > 0) {
     console.log('К сожалению у вас уровень дохода ниже среднего');
-  } else if (budgetDay  < 0) {
+  } else if (budgetDay  < 0){
     console.log('Что то пошло не так');
   }
